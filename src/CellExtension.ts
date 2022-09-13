23903d17623839c08d1621479162beb265b05ebd
ii masamitsu <ii@entap.co.jp>
2022-09-14 08:16:08 +0900

    要らない処理を削除

--- src/CellExtension.ts
+++ src/CellExtension.ts
@@ -9,9 +9,7 @@ import { DocumentRegistry } from '@jupyterlab/docregistry';
 import { IDisposable } from '@lumino/disposable';
 import {
   isMarkdownCellModel,
-  isCodeCellModel,
   MarkdownCellModel,
-  CodeCellModel,
   ICellModel,
   MarkdownCell
 } from '@jupyterlab/cells';
@@ -26,18 +24,11 @@ export class CellExtension
     context: DocumentRegistry.IContext<INotebookModel>
   ): void | IDisposable {
     widget.content.model?.cells.changed.connect((_, args) => {
-      console.log('cell changed', args);
-
       if (['add', 'set'].includes(args.type)) {
         args.newValues.forEach(c =>
           onCellAdded(c, widget.content, context.sessionContext)
         );
       }
-    });
-
-    context.sessionContext.statusChanged.connect((_, args) => {
-      // セルの実行時にbusy、実行終了時にidleに切り替わるが、どのセルかはわからない
-      console.log('session status changed', args);
     });
   }
 }
@@ -47,19 +38,9 @@ function onCellAdded(
   notebook: Notebook,
   sessionContext: ISessionContext
 ) {
-  if (isCodeCellModel(cell)) {
-    onCodeCellAdded(cell as CodeCellModel, notebook);
-  } else if (isMarkdownCellModel(cell)) {
+  if (isMarkdownCellModel(cell)) {
     onMarkdownCellAdded(cell as MarkdownCellModel, notebook, sessionContext);
   }
-}
-
-function onCodeCellAdded(cell: CodeCellModel, notebook: Notebook) {
-  cell.stateChanged.connect((_, change) => {
-    if (change.name === 'executionCount' && change.newValue === null) {
-      console.log('output cleared');
-    }
-  });
 }
 
 const memo = {} as Record<string, Widget>;
@@ -73,8 +54,10 @@ function onMarkdownCellAdded(
     | MarkdownCell
     | undefined;
   if (cell) {
+    // collapse/expandに合わせて表示・非表示を切り替える
+
     if (NotebookActions.getHeadingInfo(cell).collapsed) {
-      // FIXME そのまま実行するとうまく動かないから無理やり動かしてるけど、安定しないかも
+      // そのまま実行するとうまく動かないから無理やり動かしてるけど、安定しないかも
       setTimeout(() => {
         memo[cell.model.id] = new SectionSummaryWidget(
           cell,
@@ -84,10 +67,8 @@ function onMarkdownCellAdded(
         Widget.attach(memo[cell.model.id], cell.node);
       }, 0);
     }
-
     cellModel.metadata.changed.connect((_, args) => {
       if (args.key === 'jp-MarkdownHeadingCollapsed') {
-        console.log('toggle collapsed', args);
         if (args.type === 'add') {
           memo[cell.model.id] = new SectionSummaryWidget(
             cell,
